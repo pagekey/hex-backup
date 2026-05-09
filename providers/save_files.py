263f28f44b97199b41f49245cd91edfc86d89dde
Stephen Grice <stephengrice@live.com>
2026-05-09 12:50:57 -0400

    fix: use 777 perms on save backup

--- providers/save_files.py
+++ providers/save_files.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pathlib import Path
 from hex import Params
+import os
 
 
 def run(params: Params) -> dict[str, str]:
@@ -9,10 +10,12 @@ def run(params: Params) -> dict[str, str]:
     backup_folder_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S_GoogleDrive")
     dst_path = Path(dst_folder) / backup_folder_name
     dst_path.mkdir(parents=True, exist_ok=True)
+    os.chmod(dst_path, 0o777)
     report = []
     for file in files:
         target_path = dst_path / file.get("name")
         target_path.write_text(file.get("content"))
+        os.chmod(target_path, 0o777)
         report.append(f"Wrote {target_path.resolve()}")
     return {
         "report": report,
