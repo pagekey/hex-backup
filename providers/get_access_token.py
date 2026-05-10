5b4f5fb739071fe28c738cc598ca0d81bc8ee543
Stephen Grice <stephengrice@live.com>
2026-05-10 18:05:24 -0400

    fix: encode client secret into cron job

--- providers/get_access_token.py
+++ providers/get_access_token.py
@@ -6,9 +6,9 @@ from hex import Params
 
 
 def run(params: Params) -> dict[str, str]:
-    client_id = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
-    client_secret = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET")
-    refresh_token = params.inputs["refresh_token"]
+    client_id = params.inputs.get("client_id")
+    client_secret = params.inputs.get("client_secret")
+    refresh_token = params.inputs.get("refresh_token")
 
     response = requests.post(
         "https://oauth2.googleapis.com/token",
@@ -19,7 +19,6 @@ def run(params: Params) -> dict[str, str]:
             "grant_type": "refresh_token",
         },
     )
-    print(response.content)
     response.raise_for_status()
 
     response_json = response.json()
--- providers/schedule.py
+++ providers/schedule.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 import subprocess
 from hex import Params
@@ -7,12 +8,23 @@ def run(params: Params) -> dict[str, str]:
     src_folder = params.inputs["src_folder"]
     dst_folder = params.inputs["dst_folder"]
     daily_time = params.inputs["daily_time"]
+    client_id = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
+    client_secret = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET")
     refresh_token = params.inputs["refresh_token"]
     hour, minute = daily_time.split(":")
 
     hex_path = shutil.which("hex") or "/usr/local/bin/hex"
 
-    full_command = f"{hex_path} run hexmod-backup.run_backup -i src_folder={src_folder} -i dst_folder={dst_folder} -i refresh_token={refresh_token} -w {str(params.workspace.resolve())}".strip()
+    full_command = (
+        f"{hex_path} run "
+        "hexmod-backup.run_backup "
+        f"-i src_folder={src_folder} "
+        f"-i dst_folder={dst_folder} "
+        f"-i client_id={client_id} "
+        f"-i client_secret={client_secret} "
+        f"-i refresh_token={refresh_token} "
+        f"-w {str(params.workspace.resolve())}"
+    ).strip()
 
     cron_entry = f"{minute} {hour} * * * {full_command} # HEX_JOB:backup"
 
